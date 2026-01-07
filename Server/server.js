33d5973cf633d8e2929b6faf6c938f811208f34f
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-07 13:27:05 +0530

    Initial commit - Server Side

---
+++ Server/server.js
@@ -0,0 +1,24 @@
+// CineGo / Server / server.js
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+/* -------- INITIALIZE EXPRESS -------- */
+const app = express();
+
+/* -------- CONNECT TO DATABASE -------- */
+
+/* -------- MIDDLEWARE CONFIGURATION -------- */
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extend: true }));
+
+/* -------- ROUTES -------- */
+app.get("/", (req, res) => res.send("API is Working!"));
+
+/* -------- PORT -------- */
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
