39eb0b4b4a8bfef26c22c30177c8aa953aa8cf46
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-10 01:56:54 +0530

    import News from News/News.jsx

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 // CineGo / Client / src / pages / Home.jsx
 import Banner from "../components/Banner/Banner";
 import Movies from "../components/Movies/Movies";
-import News from "../components/News";
 import Trailers from "../components/Trailers/Trailers";
+import News from "../components/News/News";
 
 const Home = () => {
   return (
