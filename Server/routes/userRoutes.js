f335370c4c75f1a838a8b58cb55cf63ac2845807
PrethigahShanmugarajah <prethigah@gmail.com>
2026-01-11 18:42:36 +0530

    Add route for registerUser in userRouter

---
+++ Server/routes/userRoutes.js
@@ -0,0 +1,9 @@
+// CineGo / Server / routes / userRoutes.js
+import express from "express";
+import { registerUser } from "../controllers/userController.js";
+
+const userRouter = express.Router();
+
+userRouter.post("/register-user", registerUser);
+
+export default userRouter;
