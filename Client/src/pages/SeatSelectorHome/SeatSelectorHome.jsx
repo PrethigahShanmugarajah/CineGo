005c4395cbe75d02de3bf57e719fc866e67bb6ca
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-04 07:00:37 +0530

    Add the seat selector home page

---
+++ Client/src/pages/SeatSelectorHome/SeatSelectorHome.jsx
@@ -0,0 +1,12 @@
+// CineGo / Client / src / pages / SeatSelectorHome / SeatSelectorHome.jsx
+import SeatSelectorPageHome from "../../components/SeatSelectorPageHome/SeatSelectorPageHome";
+
+const SeatSelectorHome = () => {
+  return (
+    <div>
+      <SeatSelectorPageHome />
+    </div>
+  );
+};
+
+export default SeatSelectorHome;
