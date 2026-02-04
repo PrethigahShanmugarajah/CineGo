bb1565277182a99bab6ae9d25477d9efe83bac3f
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-04 07:01:47 +0530

    Add the seat selector page wrapper

---
+++ Client/src/pages/SeatSelector/SeatSelector.jsx
@@ -0,0 +1,12 @@
+// CineGo / Client / src / pages / SeatSelector / SeatSelector.jsx
+import SeatSelectorPage from "../../components/SeatSelectorPage/SeatSelectorPage";
+
+const SeatSelector = () => {
+  return (
+    <div>
+      <SeatSelectorPage />
+    </div>
+  );
+};
+
+export default SeatSelector;
