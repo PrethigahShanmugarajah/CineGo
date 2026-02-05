583e120118738deabc6c4673e33dc86397895bf9
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-05 13:25:10 +0530

    Add the add movie page wrapper

---
+++ Admin/src/pages/AddMovie.jsx
@@ -0,0 +1,12 @@
+// CineGo / Admin / src / pages / AddMovie.jsx
+import AddMoviePage from "../components/AddMoviePage";
+
+const AddMovie = () => {
+  return (
+    <div>
+      <AddMoviePage />
+    </div>
+  );
+};
+
+export default AddMovie;
