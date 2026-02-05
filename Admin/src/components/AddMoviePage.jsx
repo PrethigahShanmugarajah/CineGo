93167a3822bf71c2a3b1ded8a6551823f5a81a6a
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-05 13:24:45 +0530

    Add the add movie page component

---
+++ Admin/src/components/AddMoviePage.jsx
@@ -0,0 +1,11 @@
+// CineGo / Admin / src / components / AddMoviePage.jsx
+
+const AddMoviePage = () => {
+  return (
+    <div>
+      <h1>AddMoviePage</h1>
+    </div>
+  );
+};
+
+export default AddMoviePage;
