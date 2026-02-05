45baacd59f52bf0f8ec256d411c3d1dd412d49c0
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-05 10:03:41 +0530

    Add api route for listBookings in bookingRoutes

--- Server/routes/bookingRoutes.js
+++ Server/routes/bookingRoutes.js
@@ -1,11 +1,16 @@
 // CineGo / Server / routes / bookingRoutes.js
 import express from "express";
-import { createBooking, getBooking } from "../controllers/bookingController.js";
+import {
+  createBooking,
+  getBooking,
+  listBookings,
+} from "../controllers/bookingController.js";
 import authMiddleware from "../middlewares/auth.js";
 
 const bookingRouter = express.Router();
 
 bookingRouter.post("/booking-create", authMiddleware, createBooking);
 bookingRouter.get("/booking-get", authMiddleware, getBooking);
+bookingRouter.get("/booking-list", listBookings);
 
 export default bookingRouter;
