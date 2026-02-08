be2e960f9bca3155c44d614b1e7ebdef1cb8e858
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-08 05:59:51 +0530

    Add api route for getUsers in userRoutes

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,10 +1,15 @@
 // CineGo / Server / routes / userRoutes.js
 import express from "express";
-import { loginUser, registerUser } from "../controllers/userController.js";
+import {
+  getUsers,
+  loginUser,
+  registerUser,
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/user-register", registerUser);
 userRouter.post("/user-login", loginUser);
+userRouter.get("/user-get", getUsers);
 
 export default userRouter;
