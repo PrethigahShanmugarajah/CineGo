4e9b6bd572c7a67a4881505d4fd9b5c409a39c45
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-12 14:21:55 +0530

    Add MOVIE_GET, BOOKING_GET, BOOKING_GET_OCCUPIED, BOOKING_CREATE API route

--- Client/src/api/api_route.js
+++ Client/src/api/api_route.js
@@ -7,11 +7,18 @@ const API_ROUTES = {
     USER_LOGIN: `${BASE_URL}/api/user/user-login`,
   },
   MOVIE: {
-    MOVIE_GET_RELEASE_SOON: `${BASE_URL}/api/movie/movies-get?type=releaseSoon&limit=100`,
-    MOVIE_GET_FEATURED: `${BASE_URL}/api/movie/movies-get?type=featured&limt=6`,
-    MOVIE_GET_NORMAL: `${BASE_URL}/api/movie/movies-get?type=normal&limit=200`,
-    MOVIE_GET: `${BASE_URL}/api/movie/movies-get&limit=200`,
-    MOVIE_GET_LATEST_TRAILER: `${BASE_URL}/api/movie/movies-get?type=latestTrailers&limit=50`,
+    MOVIES_GET_RELEASE_SOON: `${BASE_URL}/api/movie/movies-get?type=releaseSoon&limit=100`,
+    MOVIES_GET_FEATURED: `${BASE_URL}/api/movie/movies-get?type=featured&limt=6`,
+    MOVIES_GET_NORMAL: `${BASE_URL}/api/movie/movies-get?type=normal&limit=200`,
+    MOVIES_GET: `${BASE_URL}/api/movie/movies-get&limit=200`,
+    MOVIES_GET_LATEST_TRAILER: `${BASE_URL}/api/movie/movies-get?type=latestTrailers&limit=50`,
+    MOVIE_GET: (id) =>
+      `${BASE_URL}/api/movie/movie-get/${encodeURIComponent(id)}`,
+  },
+  BOOKING: {
+    BOOKING_GET: `${BASE_URL}/api/booking/booking-get`,
+    BOOKING_GET_OCCUPIED: `${BASE_URL}/api/booking/booking-get-occupied`,
+    BOOKING_CREATE: `${BASE_URL}/api/booking/booking-create`,
   },
 };
 
