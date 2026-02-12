0207e77ba3525a75ee325072b4a37995c194f06e
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-12 22:06:36 +0530

    Add the verify payment page wrapper.

---
+++ Client/src/pages/VerifyPayment/VerifyPayment.jsx
@@ -0,0 +1,12 @@
+// CineGo / Client / src / pages / VerifyPayment / VerifyPayment.jsx
+import VerifyPaymentPage from "../../components/VerifyPaymentPage/VerifyPaymentPage";
+
+const VerifyPayment = () => {
+  return (
+    <div>
+      <VerifyPaymentPage />
+    </div>
+  );
+};
+
+export default VerifyPayment;
